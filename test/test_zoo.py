cea486299fddc5618daa2084418146a6379f4ff7
DestMarco <marcolino1424@gmail.com>
2024-06-05 12:31:16 +0200

    ++

--- test/test_zoo.py
+++ test/test_zoo.py
@@ -7,7 +7,7 @@ class TestZoo(TestCase):
         zooKeeper_1:ZooKeeper=ZooKeeper(name="Gianni",surname="Rossi",id=1)
         fence_1:Fence=Fence(area=100, temperature=25.0, habitat="sea")
         animal_1:Animal=Animal(name="Pluto", species="Canide", age=5, height=300.0, width=1.0,preferred_habitat="savana")
-        zooKeeper_1.dd_animal(animal_1,fence_1)
+        zooKeeper_1.add_animal(animal_1,fence_1)
         result:int=len(fence_1.animals)
         message:str=f"Errore: the fuction add_animal should not add self.animal_1 into self.fence_1 "
         self.assertEqual(result, 0, message)
